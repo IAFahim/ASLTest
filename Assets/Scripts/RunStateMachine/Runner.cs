f9e9fd343e95f9de433b4dc7b366d4bac8308e28
IAFahim.smt <iafahim.dev@gmail.com>
2025-10-15 07:59:03 +0600

    Added Platfrom and animated water

--- Assets/Scripts/RunStateMachine/Runner.cs
+++ Assets/Scripts/RunStateMachine/Runner.cs
@@ -2,5 +2,10 @@ using UnityEngine;
 
 public class Runner : MonoBehaviour
 {
-    
+    public RunnerState runnerState;
+
+    private void Update()
+    {
+        runnerState.DispatchEvent(RunnerState.EventId.DO);
+    }
 }
