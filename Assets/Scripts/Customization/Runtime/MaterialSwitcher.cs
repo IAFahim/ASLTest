708dd495fbfe755ee6f1d588881aaba9e1afe38f
IAFahim.smt <iafahim.dev@gmail.com>
2025-10-15 12:53:45 +0600

    Runner Done

---
+++ Assets/Scripts/Customization/Runtime/MaterialSwitcher.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+namespace Customization.Runtime
+{
+    public class MaterialSwitcher : MonoBehaviour
+    {
+        
+    }
+}
---
+++ Assets/Scripts/RunStateMachine/IAnimatable.cs
@@ -0,0 +1,8 @@
+namespace RunStateMachine
+{
+    public interface IAnimation
+    {
+        public void Play(string clipName);
+        public void Pause();
+    }
+}
